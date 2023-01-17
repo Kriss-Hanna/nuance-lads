67f9534901e8c0492c7137fa7614ecfedf04f873
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-01-17 20:17:15 +0100

    first render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dropdown } from "semantic-ui-react";
+
+const friendOptions = [
+  {
+    key: "Jenny Hess",
+    text: "Jenny Hess",
+    value: "Jenny Hess",
+    image: { avatar: true, src: "/images/avatar/small/jenny.jpg" },
+  },
+  {
+    key: "Elliot Fu",
+    text: "Elliot Fu",
+    value: "Elliot Fu",
+    image: { avatar: true, src: "/images/avatar/small/elliot.jpg" },
+  },
+  {
+    key: "Stevie Feliciano",
+    text: "Stevie Feliciano",
+    value: "Stevie Feliciano",
+    image: { avatar: true, src: "/images/avatar/small/stevie.jpg" },
+  },
+  {
+    key: "Christian",
+    text: "Christian",
+    value: "Christian",
+    image: { avatar: true, src: "/images/avatar/small/christian.jpg" },
+  },
+  {
+    key: "Matt",
+    text: "Matt",
+    value: "Matt",
+    image: { avatar: true, src: "/images/avatar/small/matt.jpg" },
+  },
+  {
+    key: "Justen Kitsune",
+    text: "Justen Kitsune",
+    value: "Justen Kitsune",
+    image: { avatar: true, src: "/images/avatar/small/justen.jpg" },
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>L'atelier de Stitch</h1>
+      <p>Nuancier pour faire son choix</p>
+      <Dropdown
+        placeholder="Select Friend"
+        fluid
+        selection
+        options={friendOptions}
+      />
+      <Dropdown
+        placeholder="Select Friend"
+        fluid
+        selection
+        options={friendOptions}
+      />
+
+      <a href="">Retour au site</a>
     </div>
   );
 }
