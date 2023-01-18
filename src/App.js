17f974a96b30ca7a5e112cd7ff8191c1f54afdf1
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-01-18 18:46:06 +0100

    add color and positionnement

--- src/App.js
+++ src/App.js
@@ -1,69 +1,115 @@
+import React, { useState } from "react";
 import { Dropdown } from "semantic-ui-react";
 
-const friendOptions = [
-  {
-    key: "Jenny Hess",
-    text: "Jenny Hess",
-    value: "Jenny Hess",
-    image: { avatar: true, src: "/images/avatar/small/jenny.jpg" },
-  },
-  {
-    key: "Elliot Fu",
-    text: "Elliot Fu",
-    value: "Elliot Fu",
-    image: { avatar: true, src: "/images/avatar/small/elliot.jpg" },
-  },
-  {
-    key: "Stevie Feliciano",
-    text: "Stevie Feliciano",
-    value: "Stevie Feliciano",
-    image: { avatar: true, src: "/images/avatar/small/stevie.jpg" },
-  },
-  {
-    key: "Christian",
-    text: "Christian",
-    value: "Christian",
-    image: { avatar: true, src: "/images/avatar/small/christian.jpg" },
-  },
-  {
-    key: "Matt",
-    text: "Matt",
-    value: "Matt",
-    image: { avatar: true, src: "/images/avatar/small/matt.jpg" },
-  },
-  {
-    key: "Justen Kitsune",
-    text: "Justen Kitsune",
-    value: "Justen Kitsune",
-    image: { avatar: true, src: "/images/avatar/small/justen.jpg" },
-  },
+const colorsArr = [
+  "Rouge",
+  "Rose Passion",
+  "Violet Pastel",
+  "Rose Fluo",
+  "Rose pastel",
+  "Corail",
+  "Orange",
+  "Pêche",
+  "Jaune Fluo",
+  "Jaune",
+  "Tan",
+  "Or",
+  "Vert Fluo",
+  "Vert Pastel",
+  "Vert Pomme",
+  "Caraïbe",
+  "Sarcelle",
+  "Lagune",
+  "Vert Foncé",
+  "Navy",
+  "Violet",
+  "Bleu Foncé",
+  "Bleu Pétrole",
+  "Bleu Clair",
+  "Bleu Skyan",
+  "Bleu Pastel",
+  "Beige",
+  "Militaire",
+  "Marron Clair",
+  "Lie de vin",
+  "Coyote",
+  "Marron",
+  "Marron foncé",
+  "Blanc",
+  "Gris",
+  "Noir",
+  "Émeraude",
+  "Orange Brûlée",
+  "Jaune Pastel",
+  "Saddle",
+  "Or Clair",
+  "Argent",
+  "Or Rose",
+  "Améthyste",
 ];
 
+const colorOptions = [];
+
+for (const color of colorsArr) {
+  colorOptions.push({
+    key: color,
+    text: color,
+    value: color,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZC79ojqD8W6yHpOI5SO0n4GLkv3GyR5GDjg&usqp=CAU",
+  });
+}
+
 function App() {
+  const [color1, setColor1] = useState();
+  const [color2, setColor2] = useState();
+
+  const handleOnChange = (e, data) => {
+    console.log(data);
+    setColor1(data.value);
+  };
+
   return (
-    <>
+    <div className="container-app">
       <h1>L'atelier de Stitch</h1>
       <p>Nuancier pour faire son choix</p>
+
       <Dropdown
-        placeholder="Select Friend"
-        fluid
+        placeholder="Selectionne une première couleur"
         selection
-        options={friendOptions}
+        options={colorOptions}
+        onChange={handleOnChange}
       />
       <Dropdown
-        placeholder="Select Friend"
-        fluid
+        placeholder="Selectionne une deuxieme couleur"
         selection
-        options={friendOptions}
+        options={colorOptions}
+        onChange={(e) => console.log(e)}
       />
 
       <div className="container-colors">
-        <div className="color1"></div>
-        <div className="color2"></div>
+        <img
+          className="color1"
+          /*  src={color1} */
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQMAAADDCAMAAACxkIT5AAAAA1BMVEUAAP+KeNJXAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD+BsYMAAFjd3WkAAAAAElFTkSuQmCC"
+          alt="Première couleur sélectionnée"
+        />
+        <img
+          className="color2"
+          /*  src={color2} */
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX/AAAZ4gk3AAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC"
+          alt="Deuxième couleur sélectionnée"
+        />
       </div>
 
-      <a href="www.latelierdestitch.net">Retour au site</a>
-    </>
+      <a
+        href="https://www.latelierdestitch.net/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Retour à la boutique
+      </a>
+    </div>
   );
 }
 
