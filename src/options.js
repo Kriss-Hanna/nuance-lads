a11accb105467949fd32e5570400b185a195b260
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-01-20 14:46:25 +0100

    correction ui vmobile

---
+++ src/options.js
@@ -0,0 +1,299 @@
+export const colorOptions = [
+  {
+    key: "Rouge",
+    text: "Rouge",
+    value:
+      "https://images.unsplash.com/flagged/photo-1593005510509-d05b264f1c9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVkJTIwY29sb3J8ZW58MHx8MHx8&w=1000&q=80",
+    image:
+      "https://images.unsplash.com/flagged/photo-1593005510509-d05b264f1c9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVkJTIwY29sb3J8ZW58MHx8MHx8&w=1000&q=80",
+  },
+  {
+    key: "Rose Passion",
+    text: "Rose Passion",
+    value:
+      "https://img.freepik.com/free-photo/pastel-pink-vignette-concrete-textured-background_53876-129734.jpg?w=2000",
+    image:
+      "https://img.freepik.com/free-photo/pastel-pink-vignette-concrete-textured-background_53876-129734.jpg?w=2000",
+  },
+  {
+    key: "Violet Pastel",
+    text: "Violet Pastel",
+    value:
+      "https://media.istockphoto.com/id/1273929462/fr/photo/fond-abstrait-de-mouvement-brouill%C3%A9-pourpre-defocused.jpg?s=612x612&w=0&k=20&c=-5uwtyz7A3e0aC0sPKbMW8m7f15blNuPgpMR1esOpXQ=",
+    image:
+      "https://media.istockphoto.com/id/1273929462/fr/photo/fond-abstrait-de-mouvement-brouill%C3%A9-pourpre-defocused.jpg?s=612x612&w=0&k=20&c=-5uwtyz7A3e0aC0sPKbMW8m7f15blNuPgpMR1esOpXQ=",
+  },
+  {
+    key: "Rose Fluo",
+    text: "Rose Fluo",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Rose pastel",
+    text: "Rose pastel",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Corail",
+    text: "Corail",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Orange",
+    text: "Orange",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Pêche",
+    text: "Pêche",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Jaune Fluo",
+    text: "Jaune Fluo",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Jaune",
+    text: "Jaune",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Tan",
+    text: "Tan",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Or",
+    text: "Or",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Vert Fluo",
+    text: "Vert Fluo",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Vert Pastel",
+    text: "Vert Pastel",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Vert Pomme",
+    text: "Vert Pomme",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Caraïbe",
+    text: "Caraïbe",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Sarcelle",
+    text: "Sarcelle",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Lagune",
+    text: "Lagune",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Vert Foncé",
+    text: "Vert Foncé",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Navy",
+    text: "Navy",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Violet",
+    text: "Violet",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Bleu Foncé",
+    text: "Bleu Foncé",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Bleu Pétrole",
+    text: "Bleu Pétrole",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Bleu Clair",
+    text: "Bleu Clair",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Bleu Skyan",
+    text: "Bleu Skyan",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Bleu Pastel",
+    text: "Bleu Pastel",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Beige",
+    text: "Beige",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Militaire",
+    text: "Militaire",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Marron Clair",
+    text: "Marron Clair",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Lie de vin",
+    text: "Lie de vin",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Coyote",
+    text: "Coyote",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Marron",
+    text: "Marron",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Marron foncé",
+    text: "Marron foncé",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Blanc",
+    text: "Blanc",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Gris",
+    text: "Gris",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Noir",
+    text: "Noir",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Émeraude",
+    text: "Émeraude",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Orange Brûlée",
+    text: "Orange Brûlée",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Jaune Pastel",
+    text: "Jaune Pastel",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Saddle",
+    text: "Saddle",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Or Clair",
+    text: "Or Clair",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Argent",
+    text: "Argent",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Or Rose",
+    text: "Or Rose",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Améthyste",
+    text: "Améthyste",
+    value: "",
+    image: "",
+  },
+];
+
+export const bouclerieOptions = [
+  {
+    key: "Aucune bouclerie",
+    text: "Aucune bouclerie",
+    value: "",
+    image: "",
+  },
+  {
+    key: "Or",
+    text: "Or",
+    value: "./assets/or.webp",
+    image: "./assets/or.webp",
+  },
+  {
+    key: "Acier inoxydable",
+    text: "Acier inoxydable",
+    value: "./assets/acier.webp",
+    image: "./assets/acier.webp",
+  },
+  {
+    key: "Laiton vieilli",
+    text: "Laiton vieilli",
+    value: "./assets/laiton.webp",
+    image: "./assets/laiton.webp",
+  },
+];
